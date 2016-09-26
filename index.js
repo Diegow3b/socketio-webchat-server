03ff70da9f64d73cdea5d2d6f000e8ffc2e83c3e
Diegow3bd <diegowebd@hotmail.com>
2016-09-25 21:15:23 -0300

    Added OpenShift Config

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
+
 var io = require('socket.io'),
     http = require('http'),
     server = http.createServer(),
     io = io.listen(server);
 
-io.on('connection', function(socket) {
+var user; 
+
+io.on('connection', function(socket) {       
     console.log('User Connected');
     socket.on('message', function(msg){
         io.emit('message', msg);
+    });
+    socket.on('user', function(user){        
+        this.user = user;   
     });
 });
 
 server.listen(3000, function(){
     console.log('Server started');
 })
+
+// Open Shift Config
+// var express = require('express')  
+// , app = express()
+// , server = require('http').createServer(app)
+// , io = require("socket.io").listen(server)
+// app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);  
+// app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");  
+
+// io.on('connection', function(socket) {       
+//     console.log('User Connected');
+//     socket.on('message', function(msg){
+//         io.emit('message', msg);
+//     });
+//     socket.on('user', function(user){        
+//         this.user = user;   
+//     });
+// });
+
+// server.listen(app.get('port'), app.get('ipaddr'), function(){
+// 	console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
+// });
+
